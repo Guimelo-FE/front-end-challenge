659a5534067da29100f5e7a5ad0411293f8539be
Guilherme Melo <guilhermemelo.git@gmail.com>
2021-05-01 17:43:02 -0300

    Fully functional generated products via API's endpoint

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,17 +12,23 @@ function fetchData() {
                 .map(product => {
                     return `
                     <div class="products-item">
-                        <img src="https:${product.image}" alt="Imagem" class="img"/>
-                        <h3>${product.name}</h3>
-                        <p>De: ${product.oldPrice}</p>
-                        <p>Por: ${product.price}</p>
+                        <img src="https:${product.image}" alt="Imagem" class="products-item-img"/>
+                        <div class="products-item--info">    
+                            <h4>${product.name}</h4>
+                            <p>De: R$${product.oldPrice}</p>
+                            <h3>Por: R$${product.price}</h3>
+                            <p>ou 2x de R$${parseFloat((product.price) / 2)}</p>
+                            <button>Comprar</button>
+                        </div>
                     </div>
                     `;
+
+                    //HTML generated inside the div class "products-wraper"
                 })
                 .join("");
 
-            console.log(html)
-            document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
+            document.querySelector('#products').insertAdjacentHTML('afterbegin', html);
+            //.insertAdjacentHTML ratter than .innerHTML to avoid corrupting references
         }).catch(error => {
             console.log(error);
         });
